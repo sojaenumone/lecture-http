b4c753b8c7a449344fe287fd828346cf0fd49ba8
sojaewon <kt020628@gmail.com>
2025-09-05 17:51:14 +0900

    msg

--- ch01/my-client/index.js
+++ ch01/my-client/index.js
@@ -1,1 +1,19 @@
-// todo 
+const http = require("http");
+
+const options = new URL("http://localhost:3000/");
+
+const handler = (res) => {
+ const data = [];
+
+ res.on("data", chunk => {
+    data.push(chunk.toString());
+ });
+
+ res.on("end", () => {
+    console.log(data.join(""));
+ });
+};
+
+const req = http.request(options, handler);
+
+req.end();
