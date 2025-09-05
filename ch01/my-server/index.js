a2e6d43be6231e3b542e752d76dd3ad5a0fcf913
sojaewon <kt020628@gmail.com>
2025-09-05 16:35:16 +0900

    ch01 실습 코드 추가

--- ch01/my-server/index.js
+++ ch01/my-server/index.js
@@ -1,1 +1,10 @@
-// todo 
+const http = require("http");
+
+const content = `HTTP lecture
+1. Basic
+  1.1 HTTP Start
+  1.2 HTTP Message 
+2. Web Browser
+ 2.1 Content Negotiation
+ 2.2 Cookie
+ `;
